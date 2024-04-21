6a936e34aeda84a518584eb98ae45869ac88eaed
RosalesCindy <rosalescindy829@gmail.com>
2024-04-22 00:34:47 +0800

    commit 1

---
+++ functions/api.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const router = require("./routes/author");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+const dbCloudUrl = 
+"mongodb+srv://Cindy:march2603@web-prog.4jck7t9.mongodb.net/webprogDB?retryWrites=true&w=majority&appName=web-prog";
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose
+  .connect(dbCloudUrl)
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+  })
+  .catch((err) => {
+    console.log("Error: ", err);
+  });
+
+app.use("/.netlify/functions/api", router);
+module.exports.handler = serverless(app);
